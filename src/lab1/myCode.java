4aa377bc094e6dac751f27dc08ef136e9ecf5acc
Utumporn2540 <gea24082540@gmail.com>
2018-06-26 12:19:50 +0700

    Initial commit

---
+++ src/lab1/myCode.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class myCode {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+        System.out.println("Utumporn Sriboot");
+        System.out.println("361211760014");
+        System.out.println("MIT211");
+        System.out.println("RMUTSV");
+
+}
